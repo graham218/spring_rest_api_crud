2d9eb1511ee3feecc3a173d2c322df13a0b9986a
BILL GRAHAM <grahambill011@gmail.com>
2021-09-24 04:19:31 +0300

    Update Employee.java

--- spring_rest_api/src/main/java/com/spring_rest/entity/Employee.java
+++ spring_rest_api/src/main/java/com/spring_rest/entity/Employee.java
@@ -1,5 +1,24 @@
 package com.spring_rest.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tbl_employees")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Employee {
 	
+	private Long id;
+	
+	private String name;
+	
+	private  String department;
+	
+	private String location;
 }

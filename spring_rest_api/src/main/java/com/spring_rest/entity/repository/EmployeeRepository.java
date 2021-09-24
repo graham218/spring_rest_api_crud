284c9fefb35332ec718118621180d6a61e71fed7
BILL GRAHAM <grahambill011@gmail.com>
2021-09-24 05:24:54 +0300

    Update EmployeeRepository.java

--- spring_rest_api/src/main/java/com/spring_rest/entity/repository/EmployeeRepository.java
+++ spring_rest_api/src/main/java/com/spring_rest/entity/repository/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.spring_rest.entity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.spring_rest.entity.Employee;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>{
 	
 }

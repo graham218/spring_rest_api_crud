14bec95dfc5f6d224844c7f539a27cc90957c859
BILL GRAHAM <grahambill011@gmail.com>
2021-09-25 03:52:16 +0300

    Update EmployeeController.java

--- spring_rest_api/src/main/java/com/spring_rest/entity/controller/EmployeeController.java
+++ spring_rest_api/src/main/java/com/spring_rest/entity/controller/EmployeeController.java
@@ -3,9 +3,14 @@ package com.spring_rest.entity.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +34,21 @@ public class EmployeeController {
 	@PostMapping("/employees")
 	public Employee saveEmployeeDetails(@RequestBody Employee employee) {
 		return eRepo.save(employee);
+	}
+	
+	@GetMapping("/employees/{id}")
+	public Employee getEmployeeById(@PathVariable Long id) {
+		return eRepo.findById(id).get();
+	}
+	
+	@PutMapping("/employees")
+	public Employee updateEmployee(@RequestBody Employee employee) {
+		return eRepo.save(employee);
+	}
+	
+	@DeleteMapping("/employees/{id}")
+	public ResponseEntity<HttpStatus> deleteEmployeeById(@PathVariable Long id){
+		eRepo.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 }
